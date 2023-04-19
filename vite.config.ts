f57510e4ce70d976b004f4784a8b26b86013929b
root <root@DIP>
2023-04-19 18:30:55 +0800

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: "/SEC-2023-ReactJS-week5-exer1/",
   server: {
     watch: {
       usePolling: true,
